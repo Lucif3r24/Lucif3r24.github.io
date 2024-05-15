6085c5e39f70f32e1eba5be7fd4fed58ee45e668
Lucif3r24 <adresse.sec24@gmail.com>
2024-05-15 17:30:41 +0200

    Finish the Javascript Gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,31 @@
+// Activates the image gallery.
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click.
+
 function activateGallery() {
-	alert("Hello from the gallery file!");
+
+	let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+	let mainImage = document.querySelector("#gallery-photo img");
+
+	thumbnails.forEach(thumbnail => {
+		thumbnail.addEventListener("click", function() {
+
+		// Set clicked image as main image.
+		let newImagesrc = thumbnail.dataset.largeVersion
+		mainImage.setAttribute("src", newImagesrc)
+		mainImage.setAttribute("alt", thumbnail.alt);
+
+		// Change which image is current.
+		document.querySelector(".current").classList.remove("current");
+		thumbnail.parentElement.classList.add("current");
+
+		// Update the info.
+		let galleryInfo = document.querySelector("#gallery-info");
+		let title = galleryInfo.querySelector(".title");
+		let desciption = galleryInfo.querySelector(".description");
+
+		title.innerHTML = thumbnail.dataset.title;
+		desciption.innerHTML = thumbnail.dataset.desciption;
+		});
+	});
   }
